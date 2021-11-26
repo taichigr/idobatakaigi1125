660a0f29543c98f6abc545c82b0d00d1ae70ab2a
taichi <taichi.01gr14@gmail.com>
2021-11-26 15:22:08 +0900

    save name in japanese

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -48,7 +48,7 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [ disabled, setDisabled ] = useState(true);
   const [ string, setString ] = useState('');
-  console.log({disabled, string});
+  const [ isComposed, setIsComposed ] = useState(false);
   useEffect(() => {
     const disabled = string === '';
     setDisabled(disabled)
@@ -73,12 +73,14 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e) => {
-              console.log({ key: e.key });
+              if(isComposed) return;
               if( e.key === 'Enter'){
                 setName(e.target.value);
                 e.preventDefault();
               }
             }}
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             type="button"
