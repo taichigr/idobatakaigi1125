41b9fcf371aaa2dc6422d1ea6d79aeeed2801e55
taichi <taichi.01gr14@gmail.com>
2021-11-28 17:55:00 +0900

    remove hard coded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,25 @@
 import firebase from 'firebase';
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAgfvvoQ4DYTNut1FlXHLMQI4qn3twCVzA",
-    authDomain: "chat-appliation-idobatakaigi.firebaseapp.com",
-    projectId: "chat-appliation-idobatakaigi",
-    storageBucket: "chat-appliation-idobatakaigi.appspot.com",
-    messagingSenderId: "69566290008",
-    appId: "1:69566290008:web:7d8125579b2428e1b1122d"
+
+  const {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_DATABASE_URL,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID,
+  } = process.env;
+
+
+const firebaseConfig = {
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: REACT_APP_DATABASE_URL,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
   };
 
   firebase.initializeApp(firebaseConfig);
